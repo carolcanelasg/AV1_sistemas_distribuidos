d342fe13ff066cbdb0e68d8c70307ef44a9159e8
Leticia Nunes <leticianunesq8@gmail.com>
2024-04-30 18:37:07 -0300

    adiciona conteudo de insert

--- consulta.js
+++ consulta.js
@@ -3,8 +3,14 @@ var conexao = require("./conexao.js");
 //Select from tabela
 conexao.connect(function (err) {
   if (err) throw err;
-  con.query("SELECT * FROM usuario", function (err, result, fields) {
+  conexao.query("SELECT * FROM pet", function (err, result, fields) { 
+    //alterar pra entrar tabela dinamica pela entrada de usuario 
+    //algo como ${fields.nome}
     if (err) throw err;
     console.log(result);
   });
 });
+
+
+
+
